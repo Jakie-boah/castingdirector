32d1f26b4485ae6f8f9c94af5233b051dfa1767c
Jackie_boah <VanoGalen@yandex.ru>
2022-10-18 22:42:16 +0300

    make it better

--- main.py
+++ main.py
@@ -5,15 +5,17 @@ from handlers.filter import SuperAdmin
 import bot.menu
 from Database.Database import CreateTables, TablesModerate, Database
 
-
+a = 1
 if __name__ == '__main__':
 
     logger.info("Бот запущен")
-
-    CreateTables()
-    #TablesModerate().add_new_info()
-    # print(Database().get_chats())
     dp.filters_factory.bind(SuperAdmin)
+    CreateTables()
+    if a == 1:
+        TablesModerate().add_new_info()
+        a+=1
+    # print(Database().get_chats())
+
     executor.start_polling(dp)
 
 
